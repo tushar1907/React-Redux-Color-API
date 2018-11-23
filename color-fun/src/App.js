b0766c6a7a27003affb368ef628925b10bfecb0f
Tushar <gupta.tus@husky.neu.edu>
2018-11-22 21:24:11 -0500

    Project Setup Completed

---
+++ color-fun/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BoxContainer from './containers/box-container.js'
+
+class App extends Component {
+  render() {
+    return (
+      <BoxContainer></BoxContainer>
+    );
+  }
+}
+
+export default App;
---
+++ color-fun/src/containers/box-container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BoxContainer extends Component {
+  render() {
+    return (
+      <div className="App">
+        Tushar
+      </div>
+    );
+  }
+}
+
+export default BoxContainer;
