d04b58b740b2c93160d2c2390340349ecd74d2e1
Tushar <gupta.tus@husky.neu.edu>
2018-11-22 21:38:44 -0500

    Comor button added

---
+++ color-fun/src/components/box.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Box extends Component {
+    render() {
+        return (
+            <div className='wrapper'>
+                <div className='box'>
+                    <button>Change Color</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Box;
--- color-fun/src/containers/box-container.js
+++ color-fun/src/containers/box-container.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import Box from '../components/box.js'
+
 
 class BoxContainer extends Component {
   render() {
     return (
-      <div className="App">
-        Tushar
-      </div>
+      <Box></Box>
     );
   }
 }
